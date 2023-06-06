a3def9c9dd5bab790ef229a7d2e4bd6ea0161520
Tatsuya.M <64568445+TatsuyaMaeta@users.noreply.github.com>
2023-06-06 23:10:54 +0900

    first

---
+++ main.js
@@ -0,0 +1,55 @@
+console.log(111);
+
+const url_str = "https://apis.apima.net/k2srm05wzm1pdl3xk0sv/v1/prefectures/";
+const ApiKey = "5f2fca97b5ce20ad2a668cb8efa351f1";
+// tokyo lat lon
+const lat = 35.6895;
+const lon = 139.69171;
+// api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${Ap}
+
+const elem = document.querySelector("#marker");
+const elem2 = document.querySelector("#marker2");
+
+const cityName = "tokyo";
+//api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${Ap}
+
+let hoge = null;
+
+axios
+    .get(
+        `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${ApiKey}`
+    )
+    .then(function (response) {
+        // console.log(response.data);
+        // console.log(response.status);
+        // console.log(response.statusText);
+        // console.log(response.headers);
+        console.log(response.data.name);
+        hoge = response.data.name;
+        elem.innerHTML = `<h1>${response.data.name}</h1>`;
+        return response.data.name;
+    })
+    .then((res) => {
+        const b = getWeather("osaka");
+        console.log(b);
+        // alert(res);
+        console.log(hoge, "hogeの値");
+    });
+
+const getWeather = (name) => {
+    axios
+        .get(
+            `http://api.openweathermap.org/data/2.5/weather?q=${name}&appid=${ApiKey}`
+        )
+        .then(function (response) {
+            const weather = response.data.weather[0]["main"];
+            console.log(weather);
+            hoge = weather;
+            return weather;
+        });
+};
+
+axios.get("https://api.first.org/data/v1/countries?region=asia").then((res) => {
+    console.log(res);
+    elem2.innerHTML = res
+});
